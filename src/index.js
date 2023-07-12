0d48d3fb6ff4b130951e171fb15f559e315d034a
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-07-12 20:54:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ import {  store } from 'redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-       
-            <App />
-
+        <App />
     </Provider>
   </React.StrictMode>
 );
