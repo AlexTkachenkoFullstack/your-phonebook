2be10a86f8f298e09ca194b1e42159363b03cec5
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-07-27 17:58:53 +0100

    b

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -11,7 +11,6 @@ export const FormContainer = styled(Form)`
   background: rgba(0, 0, 0, 0.1);
   border: none;
   border-radius: 20px;
-  width: 350px;
   height: auto;
   padding: 20px;
   opacity: 0.9;
@@ -100,8 +99,6 @@ export const FormButton = styled.button`
   padding-bottom: 5px;
   border-radius: 8px;
   cursor: pointer;
-  position: relative;
-  z-index: 0;
   @media screen and ${devices.xs} {
     font-size: 16px;
   }
@@ -117,61 +114,11 @@ export const FormButton = styled.button`
   color: #111;
   outline: none;
   font-family: 'Bagel Fat One', cursive;
-
-  &::before {
-    content: '';
-    background: linear-gradient(
-      45deg,
-      #ff0000,
-      #ff7300,
-      #fffb00,
-      #48ff00,
-      #00ffd5,
-      #002bff,
-      #7a00ff,
-      #ff00c8,
-      #ff0000
-    );
-    position: absolute;
-    top: -2px;
-    left: -2px;
-    background-size: 400%;
-    z-index: -1;
-    filter: blur(5px);
-    width: calc(100% + 4px);
-    height: calc(100% + 4px);
-    opacity: 0;
-    transition: opacity 0.3s easy-in-out;
-    border-redius: 10px;
-    animation: glow 15s linear infinite;
-  }
-
-  &:hover::before {
-    opacity: 1;
-  }
-
-  &::after {
-    z-index: -1;
-    content: '';
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: #fff;
-    left: 0;
-    top: 0;
-    border-radius: 10px;
-  }
-
-  @keyframes glow {
-    0% {
-      background-position: 0 0;
-    }
-    50% {
-      background-position: 400% 0;
-    }
-    100% {
-      background-position: 0 0;
-    }
+  &:hover,
+  &:focus {
+    background-color: #76a4ff;
+    scale: 1.05;
+    box-shadow: 0px 0px 14px 3px #32edd7;
   }
 `;
 
